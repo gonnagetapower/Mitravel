9cb6f4f93f9d8f835cdabcaa5ae7a0085b4eef9d
gonnagetapower <karpusha201@mail.ru>
2022-07-20 16:16:35 +0300

    fixed-bugs

--- js/main.js
+++ js/main.js
@@ -1,6 +1,39 @@
-let swiper = new Swiper('.slider-container', {
+let swiper1 = document.querySelector('.slider-container');
+let swiper2 = document.querySelector('.swiper-container')
+let burger = document.querySelector('.burger');
+let closeMenu = document.querySelector('.menu__close');
+let menu = document.querySelector('.menu');
+
+
+
+
+
+let swiperLib1 = new Swiper(swiper1, {
     centeredSlides: true,
     slidesPerView: 'auto',
     loop: true,
     spaceBetween: 105
+})
+
+let swiperLib2 = new Swiper(swiper2, {
+    centeredSlides: true,
+    slidesPerView: 1,
+    loop: true,
+    spaceBetween: 10,
+    fadeEffect: {
+        crossFade: true
+    },
+    effect: 'fade',
+    navigation: {
+        nextEl: '.btn-right',
+        prevEl: '.btn-left',
+    }
+})
+
+burger.addEventListener('click', () => {
+    menu.classList.add('menu--visible')
+})
+
+closeMenu.addEventListener('click', () => {
+    menu.classList.remove('menu--visible')
 })
